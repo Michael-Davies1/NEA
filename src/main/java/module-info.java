23814116e74f18ac4fcf9b36e43edf45bf674a1e
MD210515 <michaeldavies924@gmail.com>
2022-06-08 13:28:58 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.nea {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.nea to javafx.fxml;
+    exports com.example.nea;
+    exports;
+    opens to
+}
